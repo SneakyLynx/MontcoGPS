20852bc85538e3ae5ba7a877450e6bb1898677d2
Xearta <rsmcwow@gmail.com>
2014-04-03 13:32:45 -0400

    Added Google Maps capability

--- src/com/sneakylynx/montcogps/Main.java
+++ src/com/sneakylynx/montcogps/Main.java
@@ -1,22 +1,18 @@
 package com.sneakylynx.montcogps;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+
+/*
+ * Below is sample code to test the app. I will add the correct code tonight once I have
+ * more time!
+ */
 
 public class Main extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
