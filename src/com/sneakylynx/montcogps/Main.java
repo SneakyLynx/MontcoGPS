98eff51ecd2e8f9287b26c21e4ba2701ad204d5b
Xearta <rsmcwow@gmail.com>
2014-04-04 11:44:10 -0400

    Initial

--- src/com/sneakylynx/montcogps/Main.java
+++ src/com/sneakylynx/montcogps/Main.java
@@ -3,16 +3,42 @@ package com.sneakylynx.montcogps;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMapOptions;
+import com.google.android.gms.maps.MapFragment;
+
 /*
  * Below is sample code to test the app. I will add the correct code tonight once I have
  * more time!
  */
 
 public class Main extends Activity {
+	
+	GoogleMapOptions options = new GoogleMapOptions();
+	private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setUpMapIfNeeded();
+        mMap.setMyLocationEnabled(true);
+        options.mapType(GoogleMap.MAP_TYPE_NORMAL)
+        .compassEnabled(false)
+        .rotateGesturesEnabled(false)
+        .tiltGesturesEnabled(false);
+    }
+   
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+                                .getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                // The Map is verified. It is now safe to manipulate the map.
+
+            }
+        }
     }
 }
