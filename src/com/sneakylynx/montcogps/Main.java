9d65996554953255b0a1e736859909eea98001a6
Xearta <rsmcwow@gmail.com>
2014-04-06 10:52:03 -0400

    Got maps to work properly

--- src/com/sneakylynx/montcogps/Main.java
+++ src/com/sneakylynx/montcogps/Main.java
@@ -1,44 +1,46 @@
 package com.sneakylynx.montcogps;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.GoogleMapOptions;
-import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.SupportMapFragment;
 
 /*
  * Below is sample code to test the app. I will add the correct code tonight once I have
  * more time!
  */
 
-public class Main extends Activity {
+public class Main extends FragmentActivity {
 	
-	GoogleMapOptions options = new GoogleMapOptions();
 	private GoogleMap mMap;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setUpMapIfNeeded();
         mMap.setMyLocationEnabled(true);
-        options.mapType(GoogleMap.MAP_TYPE_NORMAL)
-        .compassEnabled(false)
-        .rotateGesturesEnabled(false)
-        .tiltGesturesEnabled(false);
     }
-   
-    private void setUpMapIfNeeded() {
-        // Do a null check to confirm that we have not already instantiated the map.
-        if (mMap == null) {
-            mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
-                                .getMap();
-            // Check if we were successful in obtaining the map.
-            if (mMap != null) {
-                // The Map is verified. It is now safe to manipulate the map.
 
-            }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+    }
+
+    private void setUpMapIfNeeded() {
+        if (mMap != null) {
+            return;
         }
+        mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+        if (mMap == null) {
+            return;
+        }
+        
+        
+        
+        // Initialize map options. For example:
+        // mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
     }
 }
